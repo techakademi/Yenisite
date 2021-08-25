09f4a08cc5109bff9876a5b5cb53f60d945fdf1c
Hamza GÜRCAN <66215655+techakademi@users.noreply.github.com>
2021-08-25 11:12:02 +0300

    Create site_PHP_giris.php

---
+++ site_PHP_giris.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Merhaba Dünya!</title>
+</head>
+<body>
+        <h1><?php echo 'Merhaba Dünya, Ben Ansible kopyalama işlemi için oluşturulmuş basit bir PHP sayfasıyım.'; ?></h1>
+
+        <h2><?php echo "Bu gün" . date("d/m/Y") . "<br>";?></h2>
+</body>
+</html>
