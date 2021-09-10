c8ca1b74b0d29befd4fd49ad9c2683975b7f7646
Hamza GÜRCAN <66215655+techakademi@users.noreply.github.com>
2021-09-10 10:25:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 <body>
         <h1><?php echo 'Merhaba Dünya, Ben Ansible kopyalama işlemi için oluşturulmuş basit bir PHP sayfasıyım.'; ?></h1>
 
-        <h2><?php echo "Bu gün". date("d/m/Y") . "<br>";?></h2>
+        <h2><?php echo "Bu günün tarihi:". date("d/m/Y") . "<br>";?></h2>
 </body>
 </html>
